df13a0b287e5ed2678c9dfbeaa3fa69859b6ab09
Aman <amakv055@gmail.com>
2021-05-23 23:11:57 +0530

    22-5-21

---
+++ lab4/ques3/Book.java
@@ -0,0 +1,7 @@
+package lab4.ques3;
+
+public class Book extends WrittenItem{
+	public Book(int identificationNumber, String title, int numberOfCopies, String author) {
+		super(identificationNumber,title,numberOfCopies,author);
+	}
+}
---
+++ lab4/ques3/MediaItem.java
@@ -0,0 +1,28 @@
+package lab4.ques3;
+
+public abstract class MediaItem extends Item{
+	
+	private int runtime;
+
+	public MediaItem(int identificationNumber, String title, int numberOfCopies, int runtime) {
+		super(identificationNumber, title, numberOfCopies);
+		this.runtime = runtime;
+	}
+
+	
+	public int getRuntime() {
+		return runtime;
+	}
+
+	
+	public void setRuntime(int runtime) {
+		this.runtime = runtime;
+	}
+	
+	@Override
+	public void print() {
+		super.print();
+		System.out.println("Runtime : "+runtime);
+	}
+
+}
